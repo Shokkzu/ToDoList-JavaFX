35ff1cab19fed0f28cd45644e8e5cd45eb6c3504
MAIGNAN_Ky <k.maignan@lprs.fr>
2022-04-07 16:18:06 +0200

    Modification : Modification de la page connexion.fxml Ajout : Ajout des actions et des variables FXML dans ConnexionController

--- src/main/java/me/kyllian/todolistjavafx/controller/ConnexionController.java
+++ src/main/java/me/kyllian/todolistjavafx/controller/ConnexionController.java
@@ -1,14 +1,24 @@
 package me.kyllian.todolistjavafx.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class ConnexionController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField mailField;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    void onConnexionButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onInscriptionButtonClick(ActionEvent event) {
+        //TODO Les méthodes changeScene et ajouter un changeScene vers la page d'inscription
     }
 }
