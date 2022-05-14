e2025ee3e64ec9c424a0fd87d02cbf8239cd696a
Shokkzu <shokkzu@gmail.com>
2022-05-14 17:07:57 +0200

    Ajout de la base de données

--- bdd/kmn_apptodolist_javafx.sql
+++ bdd/kmn_apptodolist_javafx.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : ven. 13 mai 2022 à 08:26
--- Version du serveur :  8.0.21
--- Version de PHP : 7.3.21
+-- Généré le : sam. 14 mai 2022 à 15:07
+-- Version du serveur : 5.7.36
+-- Version de PHP : 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,20 +31,13 @@ USE `kmn_apptodolist_javafx`;
 
 DROP TABLE IF EXISTS `compte`;
 CREATE TABLE IF NOT EXISTS `compte` (
-  `id_compte` int NOT NULL AUTO_INCREMENT,
+  `id_compte` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(255) NOT NULL,
   `prenom` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `mdp` varchar(255) NOT NULL,
   PRIMARY KEY (`id_compte`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `compte`
---
-
-INSERT INTO `compte` (`id_compte`, `nom`, `prenom`, `email`, `mdp`) VALUES
-(1, 'Maignan', 'Kyllian', 'k.maignan@lprs.fr', 'lprs');
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -54,17 +47,21 @@ INSERT INTO `compte` (`id_compte`, `nom`, `prenom`, `email`, `mdp`) VALUES
 
 DROP TABLE IF EXISTS `etat`;
 CREATE TABLE IF NOT EXISTS `etat` (
-  `id_etat` int NOT NULL AUTO_INCREMENT,
+  `id_etat` int(11) NOT NULL AUTO_INCREMENT,
   `etat` varchar(255) NOT NULL,
   PRIMARY KEY (`id_etat`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `etat`
 --
 
 INSERT INTO `etat` (`id_etat`, `etat`) VALUES
-(1, 'Fini');
+(1, 'Fini'),
+(2, 'En attente'),
+(3, 'Pas commencé'),
+(4, 'En pause'),
+(5, 'Abandonnée');
 
 -- --------------------------------------------------------
 
@@ -74,8 +71,8 @@ INSERT INTO `etat` (`id_etat`, `etat`) VALUES
 
 DROP TABLE IF EXISTS `gere`;
 CREATE TABLE IF NOT EXISTS `gere` (
-  `ref_tache` int NOT NULL,
-  `ref_compte` int NOT NULL,
+  `ref_tache` int(11) NOT NULL,
+  `ref_compte` int(11) NOT NULL,
   `accepte` tinyint(1) NOT NULL,
   PRIMARY KEY (`ref_tache`,`ref_compte`),
   KEY `fk_gere_compte` (`ref_compte`)
@@ -89,9 +86,9 @@ CREATE TABLE IF NOT EXISTS `gere` (
 --
 DROP VIEW IF EXISTS `infoliste`;
 CREATE TABLE IF NOT EXISTS `infoliste` (
-`id_liste` int
+`id_liste` bigint(11)
 ,`titre` varchar(255)
-,`compteTache` bigint
+,`compteTache` bigint(21)
 );
 
 -- --------------------------------------------------------
@@ -102,8 +99,11 @@ CREATE TABLE IF NOT EXISTS `infoliste` (
 --
 DROP VIEW IF EXISTS `infotache`;
 CREATE TABLE IF NOT EXISTS `infotache` (
-`id_tache` int
-,`ref_liste` int
+`nomListe` varchar(255)
+,`id_tache` int(11)
+,`ref_compte` int(11)
+,`ref_liste` int(11)
+,`accepte` tinyint(1)
 ,`nom` varchar(255)
 ,`description` varchar(255)
 ,`difficulte` varchar(255)
@@ -124,10 +124,10 @@ CREATE TABLE IF NOT EXISTS `infotache` (
 
 DROP TABLE IF EXISTS `liste`;
 CREATE TABLE IF NOT EXISTS `liste` (
-  `id_liste` int NOT NULL AUTO_INCREMENT,
+  `id_liste` int(11) NOT NULL AUTO_INCREMENT,
   `titre` varchar(255) NOT NULL,
   PRIMARY KEY (`id_liste`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -137,21 +137,21 @@ CREATE TABLE IF NOT EXISTS `liste` (
 
 DROP TABLE IF EXISTS `tache`;
 CREATE TABLE IF NOT EXISTS `tache` (
-  `id_tache` int NOT NULL AUTO_INCREMENT,
+  `id_tache` int(11) NOT NULL AUTO_INCREMENT,
   `libelle` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
   `difficulte` varchar(255) NOT NULL,
   `date_debut` date NOT NULL,
   `date_fin` date NOT NULL,
   `date_butoir` date NOT NULL,
-  `ref_type` int DEFAULT NULL,
-  `ref_etat` int DEFAULT NULL,
-  `ref_liste` int DEFAULT NULL,
+  `ref_type` int(11) DEFAULT NULL,
+  `ref_etat` int(11) DEFAULT NULL,
+  `ref_liste` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_tache`),
   KEY `fk_tache_type` (`ref_type`),
   KEY `fk_tache_etat` (`ref_etat`),
   KEY `fk_tache_liste` (`ref_liste`)
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=latin1;
 
 --
 -- Déclencheurs `tache`
@@ -176,19 +176,21 @@ DELIMITER ;
 
 DROP TABLE IF EXISTS `type`;
 CREATE TABLE IF NOT EXISTS `type` (
-  `id_type` int NOT NULL AUTO_INCREMENT,
+  `id_type` int(11) NOT NULL AUTO_INCREMENT,
   `libelle` varchar(255) NOT NULL,
-  `ref_type` int DEFAULT NULL,
+  `ref_type` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_type`),
   KEY `fk_type_type` (`ref_type`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `type`
 --
 
 INSERT INTO `type` (`id_type`, `libelle`, `ref_type`) VALUES
-(1, 'Survie', NULL);
+(1, 'Survie', NULL),
+(2, 'Managériale', NULL),
+(3, 'Projet', NULL);
 
 -- --------------------------------------------------------
 
@@ -198,7 +200,7 @@ INSERT INTO `type` (`id_type`, `libelle`, `ref_type`) VALUES
 DROP TABLE IF EXISTS `infoliste`;
 
 DROP VIEW IF EXISTS `infoliste`;
-CREATE ALGORITHM=UNDEFINED DEFINER=`apptodolist`@`%` SQL SECURITY DEFINER VIEW `infoliste`  AS  select `liste`.`id_liste` AS `id_liste`,`liste`.`titre` AS `titre`,count(distinct `tache`.`ref_liste`) AS `compteTache` from (`liste` join `tache`) ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`apptodolist`@`%` SQL SECURITY DEFINER VIEW `infoliste`  AS SELECT `liste`.`id_liste` AS `id_liste`, `liste`.`titre` AS `titre`, count(distinct `tache`.`ref_liste`) AS `compteTache` FROM (`liste` join `tache`) ;
 
 -- --------------------------------------------------------
 
@@ -208,7 +210,7 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`apptodolist`@`%` SQL SECURITY DEFINER VIEW `
 DROP TABLE IF EXISTS `infotache`;
 
 DROP VIEW IF EXISTS `infotache`;
-CREATE ALGORITHM=UNDEFINED DEFINER=`apptodolist`@`%` SQL SECURITY DEFINER VIEW `infotache`  AS  select `tache`.`id_tache` AS `id_tache`,`tache`.`ref_liste` AS `ref_liste`,`tache`.`libelle` AS `nom`,`tache`.`description` AS `description`,`tache`.`difficulte` AS `difficulte`,`tache`.`date_debut` AS `date_debut`,`tache`.`date_fin` AS `date_fin`,`tache`.`date_butoir` AS `date_butoir`,`type`.`libelle` AS `type`,`etat`.`etat` AS `etat`,`compte`.`nom` AS `nomgerant`,`compte`.`prenom` AS `prenomgerant` from ((((`tache` join `gere`) join `etat`) join `compte`) join `type`) where ((`gere`.`ref_tache` = `tache`.`id_tache`) and (`gere`.`ref_compte` = `compte`.`id_compte`) and (`etat`.`id_etat` = `tache`.`ref_etat`) and (`type`.`id_type` = `tache`.`ref_type`)) ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`apptodolist`@`%` SQL SECURITY DEFINER VIEW `infotache`  AS SELECT `liste`.`titre` AS `nomListe`, `tache`.`id_tache` AS `id_tache`, `gere`.`ref_compte` AS `ref_compte`, `tache`.`ref_liste` AS `ref_liste`, `gere`.`accepte` AS `accepte`, `tache`.`libelle` AS `nom`, `tache`.`description` AS `description`, `tache`.`difficulte` AS `difficulte`, `tache`.`date_debut` AS `date_debut`, `tache`.`date_fin` AS `date_fin`, `tache`.`date_butoir` AS `date_butoir`, `type`.`libelle` AS `type`, `etat`.`etat` AS `etat`, `compte`.`nom` AS `nomgerant`, `compte`.`prenom` AS `prenomgerant` FROM (((((`tache` join `gere`) join `etat`) join `compte`) join `type`) join `liste`) WHERE ((`gere`.`ref_tache` = `tache`.`id_tache`) AND (`gere`.`ref_compte` = `compte`.`id_compte`) AND (`etat`.`id_etat` = `tache`.`ref_etat`) AND (`type`.`id_type` = `tache`.`ref_type`) AND (`liste`.`id_liste` = `tache`.`ref_liste`)) ;
 
 --
 -- Contraintes pour les tables déchargées
